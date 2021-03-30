e3050275b22a42e15c3125848ce03635da8bcec6
Odilon Damasceno <storckskey@gmail.com>
2021-03-30 15:24:18 -0300

    feat: added some documentations to Transaction model

--- lib/src/models/transaction_model.dart
+++ lib/src/models/transaction_model.dart
@@ -1,13 +1,21 @@
+/// A classe transaction que serve para definir o modelo da transação
 class Transaction {
   bool _isDeposit;
   DateTime _date;
   double _value;
+
   Transaction({DateTime date, bool isDeposit, double value}) {
     this._date = date;
     this._isDeposit = isDeposit;
     this._value = value;
   }
+
+  /// Data da transação
   String get date => "${_date.day}/${_date.month}/${_date.year}";
+
+  /// Valor da transação
   double get value => _value;
+
+  /// Variavel para detectar se é um deposito
   bool get isDeposit => _isDeposit;
 }
